e1e9dadc935fec2d101d8f0f6ce4032225df82f0
Melissa Real <melissa.real@hotmail.com>
2019-04-22 12:44:46 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const ul = document.querySelector('.employee-list')
+  let li = document.createElement('li');
+  li.innerHTML =  retrieveEmployeeInformation();
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+    submit.addEventListener('click', function(event){
+      addNewElementAsLi()
+      document.querySelector('input').value = "";
+    })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let del = document.querySelector('a');
+  const ul = document.querySelector('.employee-list');
+
+  del.addEventListener('click', function(e) {
+      while (ul.hasChildNodes()) {
+        ul.removeChild(ul.childNodes[0]);
+      }})
+
+  }
